776a6cc7e2687007754992b9463f356b5fb9298c
AlepH-FR <lysimaque@gmail.com>
2011-05-31 09:38:21 +0200

    making the city field optionnal in registration

--- src/IHQS/NuitBlancheBundle/Entity/User.php
+++ src/IHQS/NuitBlancheBundle/Entity/User.php
@@ -67,7 +67,7 @@ class User implements UserInterface, \Serializable
     protected $lastName;
 
     /**
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", nullable="true")
      */
     protected $city;
 
