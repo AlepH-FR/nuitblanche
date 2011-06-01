33c35e15d74b6eb28d73b235f554ead5b084174c
AlepH-FR <lysimaque@gmail.com>
2011-06-02 00:32:22 +0200

    fixing main menu link

--- src/IHQS/NuitBlancheBundle/Controller/MainController.php
+++ src/IHQS/NuitBlancheBundle/Controller/MainController.php
@@ -18,6 +18,28 @@ class MainController extends Controller
             'news'  => $this->get('nb.manager.news')->findLast()
         );
     }
+    /**
+     * @Route("/to_come", name="to_come")
+     * @Template()
+     */
+    public function toComeAction()
+    {
+        return array(
+            'items' => array(
+                'v1.0' => array(
+                    'forum',
+                ),
+
+                'v2.0' => array(
+                    'comments edition',
+                    'replay notes and comments',
+                    'filters on replay and war lists',
+                    'dumb stats page',
+                    'backoffice for admins',
+                ),
+            )
+        );
+    }
 
     /**
      * @Route("/404", name="exception")
