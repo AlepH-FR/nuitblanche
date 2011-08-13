d31a0e35ff901a977217b6f91c076d4b8d9e6d3f
AlepH-FR <lysimaque@gmail.com>
2011-08-13 02:15:28 +0200

    adding languages to news form

--- src/IHQS/NuitBlancheBundle/Form/NewsFormType.php
+++ src/IHQS/NuitBlancheBundle/Form/NewsFormType.php
@@ -16,6 +16,7 @@ class NewsFormType extends AbstractType
                 'class'    => 'IHQS\NuitBlancheBundle\Entity\Team',
             ))
             ->add('teamGame')
+            ->add('lang', 'choice', array('choices' => array('uk' => 'en', 'fr' => 'fr')))
             ->add('body', new WysiwygTextareaType());
         ;
     }
